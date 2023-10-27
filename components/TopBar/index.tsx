0a8ef1cb47d6c18596820a4d09070c6018438bc0
junhea <97426534+junhea@users.noreply.github.com>
2023-10-27 17:04:30 +0900

    ✨ feat: topBar component

---
+++ components/TopBar/index.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router';
+import styles from './topBar.module.css';
+import IconButton from '../Button/IconButton';
+import { ReactNode } from 'react';
+
+interface TopBarProps {
+  backButton: boolean;
+  children?: ReactNode[];
+  actions?: ReactNode[];
+}
+
+export default function TopBar({ backButton, actions, children }: TopBarProps) {
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      {backButton ? <IconButton onClick={() => router.back()} icon="arrow_back" /> : undefined}
+      <div className={styles.content}>{children}</div>
+      <div className={styles.actions}>{actions}</div>
+    </div>
+  );
+}
