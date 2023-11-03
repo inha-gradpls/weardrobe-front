694a2b3c08956a8f232d857eea7db635c9eafa8f
junhea <97426534+junhea@users.noreply.github.com>
2023-11-03 19:24:14 +0900

    🤡 feat: mock api call

--- utils/api.ts
+++ utils/api.ts
@@ -5,5 +5,26 @@ export const BLUR_URL =
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function getHomeFeed(): Promise<HomeFeedResponse> {
-  return (await httpGet(`${API_BASE_URL}/recommendation`)).json();
+  // return (await httpGet(`${API_BASE_URL}/recommendation`)).json();
+  const products: Product[] = [];
+
+  for (let i = 0; i < 20; i++) {
+    products.push({
+      id: i,
+      sellerId: 0,
+      buyerId: 0,
+      categoryId: 0,
+      brandId: 0,
+      name: `상품${i}`,
+      price: 100000,
+      deliveryAvailable: false,
+      purchaseDate: '',
+      productImage: '',
+      description: '',
+      productStatus: '',
+      viewCount: 0,
+      createdAt: '',
+    });
+  }
+  return new Promise<HomeFeedResponse>((res) => res(products));
 }
