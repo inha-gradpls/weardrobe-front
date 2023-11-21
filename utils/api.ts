c661c313d661e374500ffaade0282db7aaf2531b
junhea <97426534+junhea@users.noreply.github.com>
2023-11-21 22:15:48 +0900

    feat: login callback api

--- utils/api.ts
+++ utils/api.ts
@@ -1,3 +1,4 @@
+import { useUser } from '@/states/user';
 import { httpGet } from './http';
 
 export const BLUR_URL =
@@ -6,4 +7,26 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function getHomeFeed(): Promise<HomeFeedResponse> {
   return (await httpGet(`${API_BASE_URL}/recommendation`)).json();
+}
+
+// returns true if redirect is needed
+export async function kakaoLoginCallback(
+  code: string,
+  state: string,
+): Promise<boolean | undefined> {
+  const res = await httpGet(`${API_BASE_URL}/login/oauth2/kakao?code=${code}&state=${state}`);
+  // error
+  if (res.status >= 400) {
+    return undefined;
+  }
+
+  // get tokens
+  const access = res.headers.get('Authorization') ?? undefined;
+  const refresh = res.headers.get('Authorization-Refresh') ?? undefined;
+
+  // set user state
+  useUser.setState({ accessToken: access, refreshToken: refresh });
+
+  // redirect(true) if no refresh
+  return refresh === undefined;
 }
