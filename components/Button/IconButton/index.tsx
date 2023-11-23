e2b83e5829115b49307d72dc97efc082fa40ee2e
junhea <97426534+junhea@users.noreply.github.com>
2023-11-23 16:56:24 +0900

    feat: style attribute for icon button

--- components/Button/IconButton/index.tsx
+++ components/Button/IconButton/index.tsx
@@ -2,19 +2,33 @@
 import styles from './iconButton.module.css';
 import 'material-symbols';
 
+type StyleTypes = 'primary' | 'secondary' | 'none';
+
 interface IconButtonProps {
   label?: string;
   icon?: string;
+  style?: StyleTypes;
   onClick: () => void;
 }
 
-export default function IconButton({ label, icon, onClick }: IconButtonProps) {
+export default function IconButton({ label, icon, onClick, style = 'none' }: IconButtonProps) {
   return (
-    <div className={styles.container} onClick={onClick}>
+    <div className={`${styles.container} ${getClassName(style)}`} onClick={onClick}>
       {icon === undefined ? undefined : (
         <span className={`material-symbols-outlined ${styles.icon}`}>{icon}</span>
       )}
       {label === undefined ? undefined : <p>{label}</p>}
     </div>
   );
+}
+
+function getClassName(style: StyleTypes) {
+  switch (style) {
+    case 'primary':
+      return styles.primary;
+    case 'secondary':
+      return styles.secondary;
+    case 'none':
+      return '';
+  }
 }
