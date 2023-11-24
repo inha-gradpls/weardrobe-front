b58892069cca0db9b30f6d6784e43be751bd50a8
junhea <97426534+junhea@users.noreply.github.com>
2023-11-24 22:00:23 +0900

    feat: add user info to user state

--- states/user.ts
+++ states/user.ts
@@ -1,19 +1,29 @@
+import { getUserInfo } from '@/utils/api';
 import { create } from 'zustand';
 
 interface UserState {
   accessToken?: string;
   refreshToken?: string;
+  userInfo?: UserInfo;
 }
+
 interface UserActions {
   reset: () => void;
+  fetch: () => void;
 }
 
 const initialState: UserState = {
   accessToken: undefined,
   refreshToken: undefined,
+  userInfo: undefined,
 };
 
 export const useUser = create<UserState & UserActions>((set) => ({
   ...initialState,
   reset: () => set(initialState),
+  fetch: async () => {
+    const res = await getUserInfo();
+    if (res === undefined) return;
+    set({ userInfo: res });
+  },
 }));
