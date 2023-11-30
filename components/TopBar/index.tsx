b8cf2c69d7f0ed8502c4b7d859518366d42ebcd8
junhea <97426534+junhea@users.noreply.github.com>
2023-12-01 00:42:39 +0900

    feat: optional onBack callback for top bar

--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -6,19 +6,24 @@ import { useRouter } from 'next/navigation';
 
 interface TopBarProps {
   backButton: boolean;
+  onBack?: () => void;
   children?: ReactNode;
   actions?: ReactNode;
   footer?: ReactNode;
 }
 
-export default function TopBar({ backButton, actions, children, footer }: TopBarProps) {
+export default function TopBar({ backButton, onBack, actions, children, footer }: TopBarProps) {
   const router = useRouter();
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        {backButton ? (
-          <IconButton style="transparent" onClick={() => router.back()} icon="arrow_back" />
-        ) : undefined}
+        {backButton && (
+          <IconButton
+            styleType="transparent"
+            onClick={onBack || (() => router.back())}
+            icon="arrow_back"
+          />
+        )}
         <div className={styles.content}>{children}</div>
         <div className={styles.actions}>{actions}</div>
       </div>
