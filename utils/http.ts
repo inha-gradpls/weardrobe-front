c77a95cc33c6377f910f92a21183576720147b26
junhea <97426534+junhea@users.noreply.github.com>
2023-12-01 23:57:03 +0900

    feat: auth http requests

--- utils/http.ts
+++ utils/http.ts
@@ -1,9 +1,18 @@
-export async function httpGet(url: string) {
-  return await fetch(url, { method: 'GET' });
+import { useUser } from '@/states/user';
+
+export async function httpGet(url: string, auth: boolean = true) {
+  const headers = new Headers();
+  if (auth) buildHeader(headers);
+  return await fetch(url, { method: 'GET', headers: headers });
 }
 
-export async function httpPost(url: string, body: any) {
+export async function httpPost(url: string, body: any, auth: boolean = true) {
   const headers = new Headers();
+  if (auth) buildHeader(headers);
   headers.set('Content-Type', 'application/json');
   return await fetch(url, { method: 'POST', body: JSON.stringify(body), headers: headers });
+}
+
+function buildHeader(headers: Headers) {
+  headers.set('Authorization', useUser.getState().accessToken ?? '');
 }
