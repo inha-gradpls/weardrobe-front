dde3318c1799544d5de96307dfcf7995bb680b31
junhea <97426534+junhea@users.noreply.github.com>
2023-12-02 23:23:39 +0900

    fix: remove old type definition

--- global.d.ts
+++ global.d.ts
@@ -1,20 +1,3 @@
-interface Product {
-  id: number;
-  sellerId: number;
-  buyerId: number;
-  categoryId: number;
-  brandId: number;
-  name: string;
-  price: number;
-  deliveryAvailable: boolean;
-  purchaseDate: string;
-  productImage: string;
-  description: string;
-  productStatus: string;
-  viewCount: number;
-  createdAt: string;
-}
-
 type HomeFeedResponse = Product[];
 
 type NavigationPage = 'HOME' | 'WARDROBE' | 'MYPAGE';
