fa64409aeebbea5ba593fd3ca5d421bdef30f719
junhea <97426534+junhea@users.noreply.github.com>
2023-12-12 12:02:58 +0900

    hotfix: follow kakao design guideline

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import styles from './page.module.css';
-import IconButton from '@/components/Button/IconButton';
 import { useUser } from '@/states/user';
 import { API_BASE_URL } from '@/utils/api';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 export default function LoginPage() {
@@ -14,11 +15,19 @@ export default function LoginPage() {
   return (
     <>
       <div className={styles.container}>
-        <IconButton
-          onClick={() => router.push(`${API_BASE_URL}/oauth2/authorization/kakao`)}
-          label="카카오 로그인"
-          icon="login"
-        />
+        <Link href={`${API_BASE_URL}/oauth2/authorization/kakao`}>
+          <Image
+            src={'/kakao_login.png'}
+            alt="카카오 로그인"
+            width={0}
+            height={40}
+            sizes="100vw"
+            style={{
+              width: '100%',
+              objectFit: 'contain',
+            }}
+          />
+        </Link>
       </div>
     </>
   );
